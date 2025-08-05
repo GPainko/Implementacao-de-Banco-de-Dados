48626bac8b2beb7395cbf7f6f20c68e2ea7063a0
Guilherme Painko Scalcon <guilherme.Painko@outlook.com>
2025-08-05 18:56:49 -0300

    revisao2

--- Revisão/SQLQuery1.sql
+++ Revisão/SQLQuery1.sql
@@ -1,5 +1,4 @@
 CREATE DATABASE Biblioteca
-
 USE Biblioteca
 
 /* Lógico_1: */
@@ -7,7 +6,7 @@ USE Biblioteca
 CREATE TABLE Livro (
     Titulo varchar(100),
     Ano Date,
-    ISBN int PRIMARY KEY,
+    ISBN varchar(50) PRIMARY KEY,
     fk_Editora_ID int,
     fk_Categoria_Codigo int
 );
@@ -29,7 +28,7 @@ CREATE TABLE Editora (
 );
 
 CREATE TABLE Livro_autor_Escrito (
-    fk_Livro_ISBN int,
+    fk_Livro_ISBN varchar(50),
     fk_Autor_IDautor int
 );
  
@@ -49,7 +48,7 @@ ALTER TABLE Livro_autor_Escrito ADD CONSTRAINT FK_Livro_autor_Escrito_1
  
 ALTER TABLE Livro_autor_Escrito ADD CONSTRAINT FK_Livro_autor_Escrito_2
     FOREIGN KEY (fk_Autor_IDautor)
-    REFERENCES Autor (IDautor);
+    REFERENCES Autor (ID);
 
 ALTER TABLE Autor
 ALTER COLUMN Nacionalidade varchar(50);
@@ -57,6 +56,7 @@ ALTER COLUMN Nacionalidade varchar(50);
 /*INSERINDO DADOS DA TABELA CATEGORIA */
 INSERT INTO Categoria(Nome) Values('Literatura Juvenil');
 INSERT INTO Categoria(Nome) Values('Humor');
+INSERT INTO Categoria(Nome) Values('Ficção Cientifica');
 
 /*INSERINDO DADOS DA TABELA AUTOR */
 INSERT INTO Autor VALUES ('J.K.ROWLING','Ingalterra');
@@ -75,12 +75,17 @@ INSERT INTO Editora(Nome) VALUES ('Belas letras');
 INSERT INTO Editora(Nome) VALUES ('Matrix');
 
 /*INSERINDO DADOS DA TABELA Livro */
+INSERT INTO Livro VALUES ('Harry Potter', '2000', 8532511015,1,1);
+INSERT INTO Livro VALUES ('As Crônicas de Nárnia', '2009', 9788578270698,2,1);
+INSERT INTO Livro VALUES ('O Espadachim de carvão', '2013', 9788577343348,3,3);
+INSERT INTO Livro VALUES ('O Papai é pop', '2015',9788581742458,4,2);
+INSERT INTO Livro VALUES ('Pior que tá não fica', '2015',9788582302026,5,2);
+INSERT INTO Livro VALUES ('Garota Desdobrável', '2015',9788577345670,3,1);
+INSERT INTO Livro VALUES ('Harry Potter e o Prisioneiro de azhaban', '2000', 8532512061,1,1);
 
-INSERT INTO Livro VALUES (;
 
 SELECT C.Codigo, C.Nome
 FROM Categoria AS C
-WHERE C.Nome LIKE '%JUVE%'
 
 SELECT A.ID, A.Nome, A.Nacionalidade
 FROM Autor AS A
@@ -90,3 +95,13 @@ FROM Editora AS E
 
 SELECT *
 FROM Livro AS L
+
+SELECT * 
+FROM Livro
+ORDER BY Titulo ASC;
+
+SELECT L.*
+FROM Livro L
+JOIN Categoria C ON L.fk_Categoria_Codigo = C.Codigo
+WHERE C.Nome = 'Literatura Juvenil'
+ORDER BY L.Ano ASC;
